c252afd3609555e0ab618201d46ca7f160590324
xupanqiang <xupq@lianliantech.cn>
2018-02-05 14:08:34 +0800

    执行 npm i --save -only=dev rc-calendar  或者 npm i D rc-calendar

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 //webpack.config.js
-var webpack = require('webpack');//引入Webpack模块供我们调用，这里只能使用ES5语法，使用ES6语法会报错
+const webpack = require('webpack');//引入Webpack模块供我们调用，这里只能使用ES5语法，使用ES6语法会报错
 
 module.exports = {
     devtool: 'eval-source-map',//生成Source Maps,这里选择eval-source-map
@@ -30,7 +30,7 @@ module.exports = {
         colors: true,//在cmd终端中输出彩色日志
         historyApiFallback: true,//在开发单页应用时非常有用，它依赖于HTML5 history API，如果设置为true，所有的跳转将指向index.html
         inline: true,//设置为true，当源文件改变时会自动刷新页面
-        port: 8083,//设置默认监听端口，如果省略，默认为"8080"
+        port: 8083,//设置默认监听端口，如果省略，默认为"8080" http://localhost:8080/
         process: true,//显示合并代码进度
     }
 };
